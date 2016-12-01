326d8c7f76a9b2e5ee703c135328426370d523a8
Maikymaik12 <maik.keijzer@gmail.com>
2016-12-01 16:28:53 +0100

    Views and Controllers: Show and New

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "dinosaurs#index"
 
   get "dinosaurs" => "dinosaurs#index"
+  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaurs
+  post "dinosaurs" => "dinosaurs#create"
   get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 end
